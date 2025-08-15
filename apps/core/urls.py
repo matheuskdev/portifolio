692be7cd4bdf8c5da7330d8962c477233505fce4
Matheus Kepy <matheuskdev@gmail.com>
2025-08-14 22:51:37 -0300

    feat: adicionar rota para a visualização de testes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import index, robots, sitemap
+from .views import index, robots, sitemap, test_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('robots.txt', robots, name='robots'),
     path('sitemap.xml', sitemap, name='sitemap'),
+    path('test/', test_view, name='test_view'),
 ]
